6d0a7fc28b86a84b6fae05963ee50da2c0c53ab7
Crytex <crytex.global@gmail.com>
2026-03-25 13:42:16 +0300

    Add Hardhat configuration for Ethereum network

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    ethereum: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
